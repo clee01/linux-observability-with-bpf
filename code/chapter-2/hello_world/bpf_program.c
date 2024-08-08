dc524ff2d8c3be374c0789f218e350d96255adb2
clee01 <1726241244@qq.com>
2024-08-08 16:18:58 +0800

    feat: chapter-2代码增加注释

--- code/chapter-2/hello_world/bpf_program.c
+++ code/chapter-2/hello_world/bpf_program.c
@@ -1,14 +1,18 @@
 #include <linux/bpf.h>
-#define SEC(NAME) __attribute__((section(NAME), used))
+#define SEC(NAME) __attribute__((section(NAME), used))  // 告知BPF虚拟机何时运行此程序
 
 static int (*bpf_trace_printk)(const char *fmt, int fmt_size,
                                ...) = (void *)BPF_FUNC_trace_printk;
 
-SEC("tracepoint/syscalls/sys_enter_execve")
+SEC("tracepoint/syscalls/sys_enter_execve")  // 该跟踪点是内核二进制代码中的静态标记
 int bpf_prog(void *ctx) {
   char msg[] = "Hello, BPF World!";
   bpf_trace_printk(msg, sizeof(msg));
   return 0;
 }
 
+// Linux内核采用GPL许可证，
+// 所以它只能加载GPL许可证程序。
+// 如果将程序设置为其他许可证，
+// 内核将拒绝加载该程序。
 char _license[] SEC("license") = "GPL";
--- code/chapter-2/hello_world/loader.c
+++ code/chapter-2/hello_world/loader.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  if (load_bpf_file("bpf_program.o") != 0) {
+  if (load_bpf_file("bpf_program.o") != 0) {  // 获取二进制文件并加载到内核中
     printf("The kernel didn't load the BPF program\n");
     return -1;
   }
 
-  read_trace_pipe();
+  read_trace_pipe();  // 读取通过`trade_printk()`或其他机制输出到跟踪管道的数据
 
   return 0;
 }
