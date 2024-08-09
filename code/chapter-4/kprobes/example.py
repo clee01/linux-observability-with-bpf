b20265907bf3291239d79f2e9d5e2658f6738043
clee01 <1726241244@qq.com>
2024-08-09 16:32:26 +0800

    feat: kprobes example.py增加注释

--- code/chapter-4/kprobes/example.py
+++ code/chapter-4/kprobes/example.py
@@ -1,17 +1,19 @@
+# -*- coding: utf-8 -*-
+
 from bcc import BPF
 
 bpf_source = """
 #include <uapi/linux/ptrace.h>
 
 int do_sys_execve(struct pt_regs *ctx) {
-  char comm[16];
+  char comm[16];  // 内核对程序命令名有16个字符的限制，所以这儿可以定义为固定长度的数组
   bpf_get_current_comm(&comm, sizeof(comm));
   bpf_trace_printk("executing program: %s\\n", comm);
   return 0;
 }
 """
 
-bpf = BPF(text=bpf_source)
-execve_function = bpf.get_syscall_fnname("execve")
-bpf.attach_kprobe(event=execve_function, fn_name="do_sys_execve")
-bpf.trace_print()
+bpf = BPF(text=bpf_source)  # 加载BPF程序到内核中
+execve_function = bpf.get_syscall_fnname("execve")  # 将BPF程序与execve系统调用关联
+bpf.attach_kprobe(event=execve_function, fn_name="do_sys_execve")  # 附加函数为attach_kprobe
+bpf.trace_print()  # 输出跟踪日志
