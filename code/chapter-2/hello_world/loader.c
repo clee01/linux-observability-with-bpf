4698caeffee651606c711b437f552e0b1f0afcb9
clee01 <1726241244@qq.com>
2024-08-09 21:52:53 +0800

    feat: 注释typo调整

--- code/chapter-2/hello_world/loader.c
+++ code/chapter-2/hello_world/loader.c
@@ -7,7 +7,7 @@ int main(int argc, char **argv) {
     return -1;
   }
 
-  read_trace_pipe();  // 读取通过`trade_printk()`或其他机制输出到跟踪管道的数据
+  read_trace_pipe();  // 读取通过`trace_printk()`或其他机制输出到跟踪管道的数据
 
   return 0;
 }
