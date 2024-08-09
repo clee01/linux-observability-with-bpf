32c6f3a65a84a51f2718b6c3a1facadcccc6cbef
clee01 <1726241244@qq.com>
2024-08-09 23:43:33 +0800

    feat: kretprobes示例增加有效注释

--- code/chapter-4/kretprobes/example.py
+++ code/chapter-4/kretprobes/example.py
@@ -5,16 +5,16 @@ bpf_source = """
 
 int ret_sys_execve(struct pt_regs *ctx) {
   int return_value;
-  char comm[16];
+  char comm[16];  // 内核对程序命令名有16个字符的限制，所以这儿可以定义为固定长度的数组
   bpf_get_current_comm(&comm, sizeof(comm));
-  return_value = PT_REGS_RC(ctx);
+  return_value = PT_REGS_RC(ctx);  // 宏PT_REGS_RC用来从特定上下文中读取BPF寄存器的返回值
 
   bpf_trace_printk("program: %s, return: %d\\n", comm, return_value);
   return 0;
 }
 """
 
-bpf = BPF(text=bpf_source)
-execve_function = bpf.get_syscall_fnname("execve")
-bpf.attach_kretprobe(event=execve_function, fn_name="ret_sys_execve")
-bpf.trace_print()
+bpf = BPF(text=bpf_source)  # 加载BPF程序到内核中
+execve_function = bpf.get_syscall_fnname("execve")  # 将BPF程序与execve系统调用关联
+bpf.attach_kretprobe(event=execve_function, fn_name="ret_sys_execve")  # 附加函数为attach_kretprobe
+bpf.trace_print()  # 输出跟踪日志
