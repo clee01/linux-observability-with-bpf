c5564ce31c2583eed55f0767ee149ba7d72f772c
clee01 <1726241244@qq.com>
2024-08-11 22:06:30 +0800

    feat: 增加有效注释

--- code/chapter-4/histograms/bcc_example.py
+++ code/chapter-4/histograms/bcc_example.py
@@ -12,8 +12,8 @@ def signal_ignore(signal, frame):
 bpf_source = """
 #include <uapi/linux/ptrace.h>
 
-BPF_HASH(cache, u64, u64);
-BPF_HISTOGRAM(histogram);
+BPF_HASH(cache, u64, u64);  // 使用BCL宏创建BPF哈希映射
+BPF_HISTOGRAM(histogram);  // 创建BPF直方图映射
 
 int trace_bpf_prog_load_start(void *ctx) {
   u64 pid = bpf_get_current_pid_tgid();
@@ -32,7 +32,7 @@ int trace_bpf_prog_load_return(void *ctx) {
     return 0;
 
   delta = bpf_ktime_get_ns() - *start_time_ns;
-  histogram.increment(bpf_log2l(delta));
+  histogram.increment(bpf_log2l(delta));  // 使用内置函数bpf_log2l为差值生成桶标识符
   return 0;
 }
 """
@@ -48,4 +48,4 @@ try:
 except KeyboardInterrupt:
     signal.signal(signal.SIGINT, signal_ignore)
 
-bpf["histogram"].print_log2_hist("msecs")
+bpf["histogram"].print_log2_hist("msecs")  # 在终端中打印包含跟踪事件分布的直方图映射
--- code/chapter-4/perf_events/bcc_example.py
+++ code/chapter-4/perf_events/bcc_example.py
@@ -3,13 +3,14 @@ from bcc import BPF
 bpf_source = """
 #include <uapi/linux/ptrace.h>
 
-BPF_PERF_OUTPUT(events);
+BPF_PERF_OUTPUT(events);  // 定义一个Perf事件映射
 
-int do_sys_execve(struct pt_regs *ctx, void filename, void argv, void envp) {
+// int do_sys_execve(struct pt_regs *ctx, void filename, void argv, void envp) {
+int do_sys_execve(struct pt_regs *ctx, char *filename, char **argv, char **envp) {
   char comm[16];
   bpf_get_current_comm(&comm, sizeof(comm));
 
-  events.perf_submit(ctx, &comm, sizeof(comm));
+  events.perf_submit(ctx, &comm, sizeof(comm));  // 更新Perf事件映射
   return 0;
 }
 """
@@ -19,16 +20,16 @@ execve_function = bpf.get_syscall_fnname("execve")
 bpf.attach_kprobe(event = execve_function, fn_name = "do_sys_execve")
 
 from collections import Counter
-aggregates = Counter()
+aggregates = Counter()  # 声明计数器来保存程序消息
 
 def aggregate_programs(cpu, data, size):
-  comm = bpf["events"].event(data)
-  aggregates[comm] += 1
+  comm = bpf["events"].event(data)  # TODO(clee01): comm is unhashable type
+  aggregates[comm] += 1  # 每当收到一个相同程序名的事件，程序计数器的值会增加
 
-bpf["events"].open_perf_buffer(aggregate_programs)
+bpf["events"].open_perf_buffer(aggregate_programs)  # 每当Perf事件映射接收到一个事件时，通知BCC需要执行aggregate_programs函数
 while True:
     try:
-      bpf.perf_buffer_poll()
+      bpf.perf_buffer_poll()  # BCC一直拉取事件直到Python程序被中断
     except KeyboardInterrupt:
       break
 
